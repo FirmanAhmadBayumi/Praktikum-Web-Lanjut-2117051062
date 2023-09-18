3c6a28e70c443de19bc4576bc6d511055d160424
Firman Ahmad Bayumi <ffirmanahmad.17@gmail.com>
2023-09-18 19:54:02 +0700

    Tugas 3 Prak

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -3,25 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Controller</title>
+    <title>Tugas 3 Prak</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url("assets/css/create_user.css")?>">
 </head>
 <body>
-    <form action="<?=base_url('/user/store')?>" method="POST">
-        <div class="nama">
-            <label>Nama :</label>
-            <input type="text" name="nama">
+    <div class="container">
+        <div class="card mt-4 col-5">
+            <div class="card-header text-center">
+                    <h2>FORM BIODATA</h2>
+            </div>
+            <div class="card-body">
+                <form method="POST" action="<?= base_url('/user/store') ?>">
+                    <div class="row mb-3">
+                        <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="nama" name="nama"
+                                placeholder="Nama" required />
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label for="kelas" class="col-sm-2 col-form-label">Kelas</label>
+                        <div class="col-sm-3">
+                            <input type="text" class="form-control" name="kelas" id="kelas" placeholder="Kelas"
+                                required />
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <label for="npm" class="col-sm-2 col-form-label">NPM</label>
+                        <div class="col-sm-4">
+                            <input type="text" class="form-control" name="npm" id="npm" placeholder="NPM" required />
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mt-3">Simpan</button>
+                </form>
+            </div>
+            <div class="card-footer text-body-secondary text-center">
+                <p> Copyright © Firman Ahmad Bayumi </p>
+            </div>
         </div>
-        <div class="npm">
-            <label>NPM :</label>
-            <input type="text" name="npm">
-        </div>
-        <div class="kelas">
-            <label>Kelas :</label>
-            <input type="text" name="kelas">
-        </div>
-        <div class="button">
-            <button>Submit</button>
-        </div>
-    </form>
+    </div>
 </body>
 </html>
